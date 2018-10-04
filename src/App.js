e2db5dfc6a8339fd9096b68914a8871ff4caf5d5
EvgeniiPresayzen <midnightzp95@gmail.com>
2018-10-05 00:33:44 +0300

    delete SearchForm

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import React, { PureComponent } from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
+import Clients from './containers/Clients/Clients';
+import Search from './components/Search/Search';
+import * as actions from './store/actions/index';
 import './App.css';
 
-import SearchForm from '../src/containers/SearchForm/SearchForm';
+export class App extends Component {
+    componentDidMount () {
+        this.props.onInitClients();
+    }
 
+    render(){
+        return (
+            <div>
+                <Search/>
+                <Clients/>
+            </div>
+        );
+    }
+};
 
-class App extends PureComponent {
-  render() {
-    return <SearchForm/>;
-  }
-}
-
-export default App;
+const mapDispatchToProps = dispatch => {
+    return {
+        onInitClients: () => dispatch(actions.initClients()),
+    }
+};
+export default connect(null, mapDispatchToProps)(App);
